f9de8f4a66fcee783f2a177268d7ab99cab3360b
Milder <milderz@gmail.com>
2025-03-19 11:39:45 -0600

    add project data for various applications

---
+++ src/projectsData.js
@@ -0,0 +1,149 @@
+export const projectsItems = [
+    {
+     "id": 1,
+     "projectName": "PhotoSnap",
+     "shortDescription": "A marketing site for a photo-sharing app, created using React JS and Styled Components.",
+     "longDescription": "For our design agency's portfolio website, we wanted to create a visually stunning and highly functional platform to showcase our work. To achieve this, we turned to React and Styled Components. \n\n Using React, we were able to build a dynamic and responsive user interface that presents our portfolio in a clean and intuitive way. We utilized React's modular design to create reusable components, making it easy to maintain and update the site as needed. \n\n Styled Components allowed us to easily apply custom styles to our React components, giving us complete control over the look and feel of the site. By using this library, we were able to create a cohesive and consistent design that truly reflects our agency's branding. \n\n Overall, the combination of React and Styled Components allowed us to build a top-quality portfolio website that effectively showcases our design work and capabilities.",
+     "mainImage": "/photo-snap.webp",
+     "secondImage": "/photo-snap.webp",
+     "thirdImage": "/photo-snap.webp",
+     "slug": "photosnap",
+     "labels": [
+      {
+       "color": "#61dbfb",
+       "text": "React",
+       "icon": "faReact"
+      },
+      {
+       "color": "#db7093",
+       "text": "STYLED COMPONENTS",
+       "icon": "faReact"
+      }
+     ],
+     "webLink":"https://wearephotosnap.netlify.app",
+     "githubLink": "https://github.com/milderz/Photosnap"
+   
+    },
+    {
+     "id": 2,
+     "projectName": "Notes App",
+     "shortDescription": "A note taking porwerful web app developed with React, Firebase and StyledComponents",
+     "longDescription": "The note taking app is a simple, yet powerful tool for organizing and storing your notes in one place. With a clean and intuitive interface, it's easy to create, edit, and delete notes as needed.\n\nOne of the key features of the app is its integration with Firebase, a powerful backend service that enables real-time data synchronization and authentication. This means that your notes are always up-to-date, no matter where you access them from.\n\nI also utilized StyledComponents to create a visually appealing and modern design for the app. With its ability to easily style and customize React components, StyledComponents allowed me to create a cohesive and visually appealing user experience.",
+     "mainImage": "/notes.png",
+     "slug": "notes-app",
+     "labels": [
+      {
+       "color": "#61dbfb",
+       "text": "React",
+       "icon": "faReact"
+      },
+      {
+       "color": "#ffcc01",
+       "text": "Firebase",
+       "icon": "faReact"
+      }
+     ],
+     "webLink":"https://thenotesappproject.netlify.app/",
+     "githubLink": "https://github.com/milderz/notes-crud-app"
+   
+    },
+    {
+     "id": 3,
+     "projectName": "Designo Studio",
+     "shortDescription": "A portfolio website for a design agency, developed using React and Styled Components",
+     "longDescription": "The website is a showcase of the design agency's work, highlighting their capabilities and expertise in various areas such as branding, web design, and print design. It features an eye-catching and modern design that showcases their unique style and creative vision.\n\nI utilized React to build a dynamic and responsive website that seamlessly adapts to different screen sizes and devices. This allows the website to provide a seamless user experience regardless of the user's device or screen size.\n\nStyled Components played a key role in the development of the website, allowing me to easily style and customize React components to create a cohesive and visually appealing design. The result is a visually stunning portfolio website that effectively communicates the design agency's capabilities and style.",
+     "mainImage": "/designo.jpg",
+     "slug": "designo-studio",
+     "labels": [
+      {
+       "color": "#61dbfb",
+       "text": "React",
+       "icon": "faReact"
+      },
+      {
+       "color": "#db7093",
+       "text": "STYLED COMPONENTS",
+       "icon": "faReact"
+      }
+     ],
+     "webLink":"https://designostudio.netlify.app/",
+     "githubLink": "https://github.com/milderz/designo"
+   
+    },
+    {
+     "id": 4,
+     "projectName": "Audiophile",
+     "shortDescription": "Audiophile is ecommerce website for a top-brand that sells headphones, earphones, and speakers.",
+     "longDescription": "The website is a showcase of the design agency's work, highlighting their capabilities and expertise in various areas such as branding, web design, and print design. It features an eye-catching and modern design that showcases their unique style and creative vision.\n\nI utilized React to build a dynamic and responsive website that seamlessly adapts to different screen sizes and devices. This allows the website to provide a seamless user experience regardless of the user's device or screen size.\n\nStyled Components played a key role in the development of the website, allowing me to easily style and customize React components to create a cohesive and visually appealing design. The result is a visually stunning portfolio website that effectively communicates the design agency's capabilities and style.",
+     "mainImage": "/audiophile.webp",
+     "slug": "audiophile",
+     "labels": [
+      {
+       "color": "#61dbfb",
+       "text": "React",
+       "icon": "faReact"
+      },
+      {
+       "color": "#db7093",
+       "text": "STYLED COMPONENTS",
+       "icon": "faReact"
+      }
+     ],
+     "webLink":"https://shopaudiophile.netlify.app/",
+     "githubLink": "https://github.com/milderz/Audiophile"
+   
+    },
+    {
+     "id": 5,
+     "projectName": "My team website",
+     "shortDescription": "Audiophile is ecommerce website for a top-brand that sells headphones, earphones, and speakers.",
+     "longDescription": "The website is a showcase of the design agency's work, highlighting their capabilities and expertise in various areas such as branding, web design, and print design. It features an eye-catching and modern design that showcases their unique style and creative vision.\n\nI utilized React to build a dynamic and responsive website that seamlessly adapts to different screen sizes and devices. This allows the website to provide a seamless user experience regardless of the user's device or screen size.\n\nStyled Components played a key role in the development of the website, allowing me to easily style and customize React components to create a cohesive and visually appealing design. The result is a visually stunning portfolio website that effectively communicates the design agency's capabilities and style.",
+     "mainImage": "/myteam.webp",
+     "slug": "myteam",
+     "labels": [
+      {
+       "color": "#d74824",
+       "text": "HTML",
+       "icon": "faReact"
+      },
+      {
+       "color": "#2760e5",
+       "text": "CSS",
+       "icon": "faReact"
+      },
+      {
+       "color": "#ead41c",
+       "text": "JAVASCRIPT",
+       "icon": "faReact"
+      }
+     ],
+     "webLink":"https://myteam-multi-page-website.netlify.app/index.html",
+     "githubLink": "https://github.com/milderz/myteam-multi-page-website"
+   
+    },{
+     "id": 6,
+     "projectName": "Buscador CABYS",
+     "shortDescription": "Buscador CABYS is an application to easily search and store CABYS Codes in one place ",
+     "longDescription": "The project in question is a web application that utilizes the API of the Ministry of Finance of Costa Rica to allow users to search for CABYS codes (Catalog of Goods and Services). The application is developed using the React JS library and styled components, which allows for an attractive and user-friendly interface.\n\nIn addition, the web application features advanced functionalities that allow users to log in and save their codes using Firebase authentication, ensuring the security of user data. Firebase Firestore is used as a database to store and retrieve saved CABYS codes.\n\nThe application is a useful tool for users who need to search for and save CABYS codes efficiently and securely, as the application allows them to access the information quickly and easily. Moreover, the use of Firebase as an authentication platform and database makes the application reliable and scalable.",
+     "mainImage": "/cabys-finder.png",
+     "slug": "buscador-cabys",
+     "labels": [
+      {
+       "color": "#61dbfb",
+       "text": "React",
+       "icon": "faReact"
+      },
+      {
+       "color": "#ffcc01",
+       "text": "Firebase",
+       "icon": "faReact"
+      }
+     ],
+     "webLink":"https://myteam-multi-page-website.netlify.app/index.html",
+     "githubLink": "https://github.com/milderz/myteam-multi-page-website"
+   
+    }
+    
+    
+    
+   ]
